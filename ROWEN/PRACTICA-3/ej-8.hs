e14c5eac2a2932f20993fa9b90f9d04c5ca1a416
Matias Rowensztein <matiasrowensztein@gmail.com>
2024-04-02 10:02:23 -0300

    .

--- ROWEN/PRACTICA-3/ej-8.hs
+++ ROWEN/PRACTICA-3/ej-8.hs
@@ -1,8 +1,6 @@
-sumaUltimosDosDigitos :: Integer -> Integer
-sumaUltimosDosDigitos c = mod c 10 + mod (c `div` 10) 10
-
 comparar :: Integer -> Integer -> Integer
 comparar a b 
     | sumaUltimosDosDigitos(a) < sumaUltimosDosDigitos(b) = 1
     | sumaUltimosDosDigitos(a) > sumaUltimosDosDigitos(b) = -1
     | sumaUltimosDosDigitos(a) == sumaUltimosDosDigitos(b) = 0
+    where sumaUltimosDosDigitos c = mod c 10 + mod (c `div` 10) 10
